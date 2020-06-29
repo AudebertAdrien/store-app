13e9a8e706a13aeefdf85d756a1f47fa02612a55
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-06-29 16:21:19 +0200

    remove email

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 # SMTP AWS SES
-ActionMailer::Base.smtp_settings = {
+=begin ActionMailer::Base.smtp_settings = {
   address: 'email-smtp.eu-west-1.amazonaws.com',
   port: 587,
   user_name: ENV['SES_SMTP_USERNAME'], # Your SMTP user
@@ -15,3 +15,4 @@ ActionMailer::Base.smtp_settings = {
   authentication: :login,
   enable_starttls_auto: true
 }
+ =end
